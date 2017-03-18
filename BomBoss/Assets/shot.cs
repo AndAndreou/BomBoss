f21aae501f845010115ee76dd59be2b4819b0095
MichalisProdromou <mixalisprodromou@gmail.com>
2017-03-18 17:04:29 +0200

    Add files via upload

---
+++ BomBoss/Assets/shot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shot : MonoBehaviour {
+
+    public float shotSpeed;
+    public float damageDealt;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 fireAhead = new Vector3(0,0,1);
+        GetComponent<Rigidbody>().velocity = fireAhead * shotSpeed;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
