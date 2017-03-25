42f63b97e7e14f6dda1db9eedfa7b81713768429
MichalisProdromou <mixalisprodromou@gmail.com>
2017-03-25 12:13:43 +0200

    Add files via upload

---
+++ BomBoss/Assets/Scripts/MichalisScene/ShipController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    //for firing
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate, nextFire;
+    public int overheatRate, overheatTotal;
+    public float cooldownRate;
+    public bool isOverheated = false;
+
+    public Transform shipSpawn; // Spawn position
+
+    // Use this for initialization
+    void Start () {
+
+        //0.1seconds after the game starts the function " cannonColldown " will be called.
+        //The function will be called every X seconds (as set by the "cooldownRate" variable in the player inspector)
+        InvokeRepeating("cannonCooldown", 0.1f, cooldownRate);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1") && (Time.time > nextFire) && (isOverheated == false))
+        {
+            nextFire = Time.time + fireRate; //Set the next available time for firing a missile
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); //spawn a missile
+            overheatTotal += overheatRate; //Everytime the cannon fires, it's heat rises
+
+            //check if the cannon has reached maximum overheat and needs to cool down
+            if (overheatTotal >= 100)
+            {
+                overheatTotal = 100;
+                isOverheated = true;
+            }
+        }
+
+    }
+
+    public void Die()
+    {
+       // print("this works");
+               
+        gameObject.SetActive(false);
+       
+        // Move it to the spawn point
+        this.transform.position = shipSpawn.position;
+        //yield return new WaitForSeconds(seconds);
+
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        this.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+       
+        gameObject.SetActive(true);
+       
+    }
+
+    //used for cannon mechanics
+    void cannonCooldown()
+    {
+        overheatTotal -= 10;//reduce the overheat every second
+        //When overheat reaches 0 (or below 0) then set the counter to 0 and change the flag. Player can now fire again
+        if (overheatTotal <= 0)
+        {
+            overheatTotal = 0;
+            isOverheated = false; // not the player can shoot with the cannon again
+        }
+    }
+
+
+}//END OF MONOBEHAVOUR
---
+++ BomBoss/Assets/Scripts/MichalisScene/ShipStatus.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipStatus : MonoBehaviour {
+
+    ShipController shipController;
+    MovementEngine movementEngine;
+    Rigidbody rigidbody;
+
+    //for Healing
+    public float maxHealth;
+    public float currHealth;
+
+    //for jumping
+    Vector3 jumpVector;
+    public float jumpForce;
+    public float jumpsLeft;
+    public bool hasJump = true;
+
+    //for shield
+    public bool hasShield = false;
+    public float shieldtDuration;
+    public float maxShieldHealth;
+    public float currShieldHealth;
+
+    //for boost
+    public bool hasBoost = true;
+    public float boostDuration;
+
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        jumpVector = new Vector3(0.0f, 1.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+
+        //Check input from user to activate the power-ups
+        if (Input.GetKeyDown("b") && hasBoost)
+        {
+            print("B key was pressed, activated boost");
+            StartCoroutine(boostMe());
+        }
+
+        if (Input.GetKeyDown("n") && hasJump && jumpsLeft > 0)
+        {
+            print("N key was pressed, activated jump");
+            jumpsLeft--;
+            rigidbody.AddForce(jumpVector * jumpForce, ForceMode.Impulse);// TO DO: make the jump smoother
+        }
+
+        if (Input.GetKeyDown("m") && hasShield)
+        {
+            print("M key was pressed, activated shield");
+            StartCoroutine(shieldMe());
+        }
+
+    }//END OF FIXED UPDATE
+
+
+    //Used to substruct health from the player and check if his HP reaches zero
+    public void applyDamage(float damage)
+    {
+        currHealth -= damage;
+        if (currHealth <= 0)
+        {
+            currHealth = 0;
+            shipController.Die();           
+        }
+    }
+
+    //Boost mechanics
+    IEnumerator boostMe()
+    {
+        float timePassed = 0; //Used for time counting
+
+        while (timePassed <= boostDuration) //check if the boost duration is over
+        {
+            movementEngine.MaxSpeed = 40;
+            movementEngine.MaxForwardAcceleration = 40;
+            //accelaration = 5; //Had to use a hard-coded value. I tried to do: accelaration *= 5; but the value hit infinity. TODO find a better solution for this
+            timePassed += Time.deltaTime; // increment our timer
+            yield return null;
+        }
+
+        while (timePassed > 0) //used to return the value back to its original value over time
+        {
+            movementEngine.MaxSpeed = 25;
+            movementEngine.MaxForwardAcceleration = 20;
+           // accelaration = 2; //TODO: find a better solution for this that is not hardcoded in
+            timePassed -= Time.deltaTime;
+            yield return null;
+        }
+        hasBoost = false;
+
+    }//End of boostME IEnumerator
+
+    //Shield mechanics
+    IEnumerator shieldMe()
+    {
+        float shieldTimePassed = 0; //Used for time counting
+
+        while (shieldTimePassed <= shieldtDuration) //check if the boost duration is over
+        {
+            //TODO: Check if player was hit by a missile or AI bot
+
+            //when shield cannot take any more hits, deativate it
+            if (currShieldHealth <= 0)
+            {
+                hasShield = false; //TODO: Will the shield be a particle / object? If so then does it need to be set: active=false ?
+            }
+            yield return null;
+        }
+        hasShield = false;
+    }//End of shieldME IEnumerator
+
+
+
+
+
+}
